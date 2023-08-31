2fa50283a364de7dad93154b5d0210d4afccb71b
Solpostgit <begemot_off@mail.ru>
2023-08-31 19:48:40 +0300

    Added task1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
+    public static int countsLeapYear (int year) {
+        Boolean leapYear = (year % 4 == 0) && (year % 100 != 0 ) || (year % 400 == 0); // определяем високосный год, как
+        // каждый четвертый и не каждый сотый, или каждый четырёхсотый.
+        if (leapYear) {
+            System.out.printf("%d год является високосным \n", year);
+        }
+        else
+            System.out.printf("%d год не является високосным \n", year);
+        return year;
+    }
     public static void main(String[] args) {
         task1();/*
         task2();
@@ -7,5 +17,6 @@ public class Main {
 
     public static void task1() {
         System.out.println("Task 1");
+        countsLeapYear(2021);
     }
 }
