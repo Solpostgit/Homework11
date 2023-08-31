932ecc4197d45662c57d4f440f25cfc149fd1b26
Solpostgit <begemot_off@mail.ru>
2023-08-31 20:50:45 +0300

    Added task2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,40 @@
 public class Main {
-    public static int countsLeapYear (int year) {
-        Boolean leapYear = (year % 4 == 0) && (year % 100 != 0 ) || (year % 400 == 0); // определяем високосный год, как
+    public static int countsLeapYear(int year) {
+        Boolean leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0); // определяем високосный год, как
         // каждый четвертый и не каждый сотый, или каждый четырёхсотый.
         if (leapYear) {
             System.out.printf("%d год является високосным \n", year);
-        }
-        else
+        } else
             System.out.printf("%d год не является високосным \n", year);
         return year;
     }
+
+    public static void changeOS(int clientOS, int clientDeviceYear) {
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else System.out.println("Установите версию приложения для Android по ссылке");
+        } else
+            System.out.println("Используйте телефон с iOS или Android");
+    }
+
     public static void main(String[] args) {
-        task1();/*
-        task2();
+        task1();
+        task2();/*
         task3();*/
     }
 
     public static void task1() {
         System.out.println("Task 1");
         countsLeapYear(2021);
+    }
+
+    public static void task2() {
+        System.out.println("Task 2");
+        changeOS(1, 2023);
     }
 }
